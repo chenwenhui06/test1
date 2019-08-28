f7925f6d2d1b5ce5f9c6efab867dc8c5ee26f13a
Administrator <Administrator@WIN10-806231051>
2019-08-28 09:45:29 +0800

    1

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ package com.tedu.sp03.user.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -13,6 +14,7 @@ import com.tedu.sp01.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
